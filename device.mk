0de07ed9b0f318f2b4feef0521c261de7571fc86
Demon000 <demonsingur@gmail.com>
2020-07-20 00:26:57 +0300

    chopin: Build fastbootd

--- device.mk
+++ device.mk
@@ -102,6 +102,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+		    fastbootd
+
 # Rootdir
 PRODUCT_PACKAGES += \
 	 fstab.mt6893 \
