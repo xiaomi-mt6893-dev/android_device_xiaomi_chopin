38271f72a20290ee78f4b85b99729bd201e16561
bengris32 <bengris32@protonmail.ch>
2023-07-18 19:47:40 +0100

    chopin: Include `hardware/mediatek` Soong namespace
    Change-Id: I9028cec415568445e4bf6b2cb7cdf3f03e95154d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -60,7 +60,8 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Kernel Build Options
 BUILD_SOURCE_KERNEL=no
