990a3ea667ea2e250f7ceac1a93779fefedcbc02
SamarV-121 <samarvispute121@pm.me>
2023-07-20 14:52:21 +0100

    chopin: Link older libutils for PQ HAL
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Change-Id: I7ea86d3d7c68ba183b4838cb1f35a9fc58cfbf67

--- device.mk
+++ device.mk
@@ -165,7 +165,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
     android.hardware.graphics.composer@2.3-service
 PRODUCT_PACKAGES += \
-    libdrm.vendor
+	libdrm.vendor \
+    libutils-v32
 
 # Power
 PRODUCT_PACKAGES += \
