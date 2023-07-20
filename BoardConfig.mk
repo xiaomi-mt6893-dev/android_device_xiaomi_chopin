065fe2896581425cf9e9d9317ce82d2af5948bd8
bengris32 <bengris32@protonmail.ch>
2023-07-20 19:41:29 +0100

    ares: Generate Framework compatibility matrix
    * Made with https://gist.github.com/SebaUbuntu/ec053a00a9988eaea091fb1cc1f19324
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Id26f665242972c7724425d3710e2b4d54a011a4c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,8 @@ TARGET_BOOTLOADER_BOARD_NAME := chopin
 TARGET_NO_BOOTLOADER := true
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
