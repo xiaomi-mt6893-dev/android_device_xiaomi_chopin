f38fc7da44d297a169d72c29fc11aa2b05831578
SamarV-121 <samarvispute121@pm.me>
2023-07-23 22:28:30 +0100

    chopin: Use libutils-v32 for libmtkcam_stdutils.so
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Change-Id: I8e4466f68660ea5dca23a51e624e46561cd73d10

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,9 @@ function blob_fixup {
 		   vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
 			"$PATCHELF" --replace-needed "android.hardware.power-V2-ndk_platform.so" "android.hardware.power-V2-ndk.so" "$2"
 			;;
+		   vendor/lib*/libmtkcam_stdutils.so)
+			"${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
+			;;
 	    esac
     }
 
