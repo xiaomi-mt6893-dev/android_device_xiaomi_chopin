43320e6339afceb9324008d13f40ab5ec3d25dc2
Giovanni Ricca <giovanniricca@proton.me>
2023-08-24 02:46:30 +0200

    chopin: Call lineage framework_compatibility_matrix from vendor/lineage
    Change-Id: I86552803fa67fbdacb1383755bd60b5ef0df8ec8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,8 +44,10 @@ TARGET_NO_BOOTLOADER := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/framework_compatibility_matrix.xml \
-    $(DEVICE_PATH)/mediatek_framework_compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+    $(DEVICE_PATH)/mediatek_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml 
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Partitions
