cd6b101d18714349b4d665df404fc92efb339601
rio004 <riyon336@gmail.com>
2023-09-28 10:43:29 +0100

    chopin: Import Bluetooth stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/blob/master/vendor/bt/proprietary-files.txt
    
    Co-authored-by: electimon <electimon@gmail.com>

--- device.mk
+++ device.mk
@@ -114,7 +114,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.3-impl:32
+    android.hardware.soundtrigger@2.3-impl:32 \
+    audio.bluetooth.default
 PRODUCT_PACKAGES += \
     libaudiofoundation.vendor \
     libalsautils \
@@ -129,6 +130,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth@1.1.vendor \
+    libbluetooth_audio_session
 
 # Rootdir
 PRODUCT_PACKAGES += \
