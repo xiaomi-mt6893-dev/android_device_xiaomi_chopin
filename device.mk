8d8af64aa2f8248da9d82abdff5b7ea3ddf72708
rio004 <riyon336@gmail.com>
2023-09-28 13:41:34 +0100

    chopin: Import Keymaster stack

--- device.mk
+++ device.mk
@@ -140,7 +140,15 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth@1.1.vendor \
     libbluetooth_audio_session
-
+	
+# Keymaster
+PRODUCT_PACKAGES += \
+		android.hardware.keymaster@4.0.vendor:64 \
+		android.hardware.keymaster@4.0.vendor \
+		libkeymaster4support.vendor \
+		libkeymaster4.vendor \
+		libpuresoftkeymasterdevice.vendor \
+		libsoft_attestation_cert.vendor
 
 # Display
 PRODUCT_PACKAGES += \
