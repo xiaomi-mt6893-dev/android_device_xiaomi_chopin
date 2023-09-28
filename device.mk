99c201b715ac5d5bb69575190e3a579ac80ec50d
rio004 <riyon336@gmail.com>
2023-09-28 14:44:18 +0100

    chopin: Configure Media stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/blob/master/vendor/media-c2/proprietary-files.txt
    
    Co-authored-by: electimon <electimon@gmail.com>

--- device.mk
+++ device.mk
@@ -153,7 +153,20 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 		  libshim_libkeymaster4 
 
+# Media
+PRODUCT_PACKAGES += \
+    libavservices_minijail_vendor \
+    libcodec2_hidl@1.2.vendor \
+    libcodec2_soft_common.vendor \
+    libsfplugin_ccodec_utils.vendor \
+    libstagefright_softomx_plugin.vendor
 
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # Permissions
 PRODUCT_COPY_FILES += \
