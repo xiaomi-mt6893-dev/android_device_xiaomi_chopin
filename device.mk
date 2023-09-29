c9b91699be9494edf1c7d599137b18655d378960
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-09-29 22:16:13 +0100

    chopin: Build common Xiaomi light AIDL

--- device.mk
+++ device.mk
@@ -154,6 +154,10 @@ PRODUCT_PACKAGES += \
 		libkeymaster4.vendor \
 		libpuresoftkeymasterdevice.vendor \
 		libsoft_attestation_cert.vendor
+		
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.xiaomi
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
