7e8e16577ab3a6b6e4e5768244c4e42ad1a10763
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-09-29 21:29:13 +0100

    chopin: Import GNSS stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/tree/master/vendor/gnss
    
    Co-authored-by: electimon <electimon@gmail.com>
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -214,6 +214,12 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@2.0.vendor
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gnss-V1-ndk.vendor \
+    android.hardware.gnss@2.1.vendor
 
 # USB
 PRODUCT_PACKAGES += \
