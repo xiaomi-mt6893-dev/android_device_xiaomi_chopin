eedbe73b2b8a2a946a87524aefffb7596842ccd2
rio004 <riyon336@gmail.com>
2023-12-11 23:20:34 +0000

    chopin: Switch to common Memtrack service
    Signed-off-by: rio004 <riyon336@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -65,10 +65,6 @@ function blob_fixup {
 		    vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
             		"$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
 	                ;;
-		    vendor/bin/hw/android.hardware.memtrack-service.mediatek |\
-		    vendor/lib64/hw/android.hardware.memtrack-impl-mediatek.so)
-		        "$PATCHELF" --replace-needed "android.hardware.memtrack-V1-ndk_platform.so" "android.hardware.memtrack-V1-ndk.so" "$2"
-			;;
 		   vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
 			"$PATCHELF" --replace-needed "android.hardware.power-V2-ndk_platform.so" "android.hardware.power-V2-ndk.so" "$2"
 			;;
