2f5dd4a8f89cc5ee515af9acc3ecbd6dc16512d6
bengris32 <bengris32@protonmail.ch>
2023-12-19 15:26:53 +0000

    chopin: Switch to AOSP ConsumerIr service
    * The required commits have been merged in AOSP.
    
    Change-Id: I686a406555bd38c4ee64f4840ca1e3f807552fbd
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -153,8 +153,7 @@ PRODUCT_PACKAGES += \
 
 # ConsumerIr
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.example
 
 # Keymaster
 PRODUCT_PACKAGES += \
