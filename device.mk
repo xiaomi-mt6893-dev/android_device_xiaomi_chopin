c05363058258af618eb346a4c4ead2b41f64696e
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-12-29 02:13:34 +0000

    chopin: Move to health AIDL services
    Change-Id: Ibadf22d432791551c16d7f4e386b470574c390f8

--- device.mk
+++ device.mk
@@ -30,9 +30,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-	   android.hardware.health@2.1-impl \
-	   android.hardware.health@2.1-impl.recovery \
-	   android.hardware.health@2.1-service
+	android.hardware.health-service.mediatek \
+	android.hardware.health-service.mediatek-recovery	
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
