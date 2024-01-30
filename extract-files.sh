b1bd2c2036f4afaa230cae656b8996321a0500d1
rio004 <riyon336@gmail.com>
2024-01-30 18:34:35 +0000

    fixup! chopin: Add a shim to link sensor blobs against android.hardware.sensors@1.0-convert
    Signed-off-by: rio004 <riyon336@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -72,8 +72,8 @@ function blob_fixup {
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
 	    ;;
         vendor/bin/mnld|\
-        vendor/lib*/hw/android.hardware.sensors@2.X-subhal-mediatek.so|\
-        vendor/lib*/libaalservice.so)
+        vendor/lib*/libaalservice.so|\
+        vendor/lib64/libcam.utils.sensorprovider.so)
             "$PATCHELF" --add-needed "libshim_sensors.so" "$2"
             ;;
         system_ext/lib64/libsink.so)
