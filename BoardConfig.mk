2d7e00ebb03b8e8f12a7752698ecb497582988b1
ZiadTamer <ziadtamer150@gmail.com>
2024-07-13 05:32:06 +0300

    chopin: Configure Virtual A/B OTAs
    Change-Id: I47b98a2be724561e775db9e78e65ec7322017daa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,19 @@
 
 DEVICE_PATH := device/xiaomi/chopin
 
+# A/B
+AB_OTA_UPDATER := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    system \
+    system_ext \
+    product \
+    vendor \
+    vbmeta
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
--- device.mk
+++ device.mk
@@ -4,6 +4,34 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit virtual_ab_ota product
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# A/B
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
