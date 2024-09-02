05b6abb09fd12559bd747a2bc91a116c358eeb58
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-09-02 09:33:41 +0000

    Set SELinux to Permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,6 +90,7 @@ BOARD_RAMDISK_OFFSET := 0x11088000
 BOARD_KERNEL_TAGS_OFFSET := 0x07c08000
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 ifeq ($(PREBUILT_DTB),yes)
 	TARGET_PREBUILT_DTB := $(DEVICE_PATH)-kernel/dtb.img
