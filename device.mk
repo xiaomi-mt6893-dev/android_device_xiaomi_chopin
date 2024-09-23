640f6025ac26f3627a5bfafa934f7aa7c2622ddb
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-09-23 14:08:36 +0000

    chopin: Build DRM HAL Services

--- device.mk
+++ device.mk
@@ -75,6 +75,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 		        libdrm.vendor 
 
+# DRM
+PRODUCT_PACKAGES += \
+	     android.hardware.drm-service.clearkey
+
+PRODUCT_PACKAGES += \
+		        android.hardware.drm@1.4.vendor
+
+PRODUCT_PACKAGES += \
+		        libdrmclearkeyplugin \
+			libmockdrmcryptoplugin
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
