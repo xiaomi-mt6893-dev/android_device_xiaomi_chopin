bcec0fd64c611bcd942e232b8447d739f5e852fe
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-09-23 15:27:37 +0000

    chopin: Build Thermal HAL Service

--- device.mk
+++ device.mk
@@ -177,6 +177,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	     android.hardware.secure_element@1.2.vendor
 
+
+# Thermal
+ PRODUCT_PACKAGES += \
+		     android.hardware.thermal@1.0-impl \
+		     android.hardware.thermal@2.0.vendor
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
