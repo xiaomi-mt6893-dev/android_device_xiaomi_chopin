5a9a3985ca4c27e61437a5e93652da8923142249
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-09-23 14:25:22 +0000

    chopin: Configure HIDL Packages

--- device.mk
+++ device.mk
@@ -91,6 +91,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 		        android.hardware.neuralnetworks@1.3.vendor \
 			 libtextclassifier_hash.vendor
+# HIDL
+PRODUCT_PACKAGES += \
+		  libhidltransport \
+		  libhidltransport.vendor \
+		  libhwbinder \
+		  libhwbinder.vendor \
+		  android.hidl.allocator@1.0.vendor
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
