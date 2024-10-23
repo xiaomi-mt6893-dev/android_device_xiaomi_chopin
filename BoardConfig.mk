b204a660c00b82430e76d7cfedf494ac67f013af
ZiadTamer <ziadtamer150@gmail.com>
2024-10-23 15:54:31 +0300

    chopin: Import FCM from `hardware/mediatek`

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,10 @@ TARGET_NO_BOOTLOADER := true
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/mediatek_framework_compatibility_matrix.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
 
 # Partitions
 
