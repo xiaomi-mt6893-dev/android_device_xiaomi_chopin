97bd50e31171ef51d42511ce7d40ba541c43baa8
ZiadTamer <ziad@android.com>
2024-10-25 10:03:04 +0000

    chopin: Add TEE Prop to CMDLINE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,7 @@ BOARD_KERNEL_TAGS_OFFSET := 0x07c08000
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
 
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.init_fatal_reboot_target=recovery androidboot.tee_type=2
 
 TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)-kernel/kernel
