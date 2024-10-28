f92249bfdd5c144c9f50e6f4f4d6c6d0cc1bfd0d
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-10-28 15:31:17 +0000

    chopin: Uprev hal drm.clearkey to 1.4 10-27 17:06:27.780   677   677 F DEBUG   : Cmdline: /vendor/bin/hw/android.hardware.drm@1.4-service.clearkey 10-27 17:06:27.780   677   677 F DEBUG   : pid: 653, tid: 653, name: android.hardwar  >>> /vendor/bin/hw/android.hardware.drm@1.4-service.clearkey <<< 10-27 17:06:27.780   677   677 F DEBUG   : uid: 1013 10-27 17:06:27.780   677   677 F DEBUG   : signal 6 (SIGABRT), code -1 (SI_QUEUE), fault addr -------- 10-27 17:06:27.780   677   677 F DEBUG   : Abort message: 'Check failed: drmFactory->registerAsService("clearkey") == android::NO_ERROR (drmFactory->registerAsService("clearkey")=-2147483648, android::NO_ERROR=0) Failed to register Clearkey Factory HAL' 10-27 17:06:27.780   677   677 F DEBUG   :     x0  0000000000000000  x1  000000000000028d  x2  0000000000000006  x3  0000007ffa125570 10-27 17:06:27.780   677   677 F DEBUG   :     x4  7372686664713d2c  x5  7372686664713d2c  x6  7372686664713d2c  x7  7f7f7f7f7f7f7f7f 10-27 17:06:27.780   677   677 F DEBUG   :     x8  00000000000000f0  x9  00000078e0d799e0  x10 0000000000000001  x11 00000078e0dbb360 10-27 17:06:27.780   677   677 F DEBUG   :     x12 0000007ffa1254f0  x13 00000000000000c2  x14 b40000776a45b850  x15 002b64660171b0a0 10-27 17:06:27.780   677   677 F DEBUG   :     x16 00000078e0e27d50  x17 00000078e0e03e70  x18 00000078e30a6000  x19 000000000000028d 10-27 17:06:27.780   677   677 F DEBUG   :     x20 000000000000028d  x21 00000000ffffffff  x22 00000078e2b70010  x23 0000005f0a9ac96f 10-27 17:06:27.780   677   677 F DEBUG   :     x24 0000005f0a9ace6b  x25 0000005f0a9ad315  x26 0000005f0a9aca63  x27 0000005f0a9ad3ed 10-27 17:06:27.780   677   677 F DEBUG   :     x28 0000005f0a9ad786  x29 0000007ffa1255f0 10-27 17:06:27.780   677   677 F DEBUG   :     lr  00000078e0dab1b8  sp  0000007ffa125550  pc  00000078e0dab1e4  pst 0000000000001000

--- device.mk
+++ device.mk
@@ -95,10 +95,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-	     android.hardware.drm-service.clearkey
-
-PRODUCT_PACKAGES += \
-		        android.hardware.drm@1.4.vendor
+	     android.hardware.drm@1.4-service.clearkey \
+	     android.hardware.drm@1.4.vendor
 
 PRODUCT_PACKAGES += \
 		        libdrmclearkeyplugin \
