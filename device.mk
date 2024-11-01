9af4965c0069bd211ce0db539228420f960f3dff
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-11-01 08:56:02 +0000

    [DNM] Fix MTKPower

--- device.mk
+++ device.mk
@@ -240,8 +240,6 @@ PRODUCT_PACKAGES += \
 			android.hardware.power@1.3.vendor
 
 PRODUCT_PACKAGES += \
-		    vendor.mediatek.hardware.mtkpower@1.0.vendor \
-		    vendor.mediatek.hardware.mtkpower@1.1.vendor \
 		    vendor.mediatek.hardware.mtkpower@1.2.vendor
 
 # Radio
