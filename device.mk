eb41b6160f24f24563d2692e46545920ec12ee07
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-11-02 04:43:15 -0700

    chopin: Kang overlay from `amber`
    https://github.com/xiaomi-mt6893-dev/android_device_xiaomi_amber/tree/lineage-21/overlay-lineage
    https://github.com/xiaomi-mt6893-dev/android_device_xiaomi_amber/tree/lineage-21/rro_overlays

--- device.mk
+++ device.mk
@@ -53,6 +53,19 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 	
+ 
+# Overlays
+PRODUCT_PACKAGES += \
+    SettingsResOverlayAmber \
+    SystemUIOverlayAmber \
+    WifiOverlay \
+    CarrierConfigOverlayAmber \
+    FrameworksResOverlayAmber \
+    TelephonyResOverlayAmber \
+    TetheringResOverlayAmber
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+    
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
