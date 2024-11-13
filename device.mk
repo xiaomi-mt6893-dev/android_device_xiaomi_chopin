981df533526e6f194b99989bcf264ef53d9b9cbd
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-11-13 16:32:40 +0530

    device-mk: Fix a Typo

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service
 
-Health
+# Health
 PRODUCT_PACKAGES += \
 	   android.hardware.health@2.1-impl \
 	   android.hardware.health@2.1-impl.recovery \
