87759c910b7632e9b6fc03f8655097e601285528
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-11-24 06:54:52 +0000

    merge derp fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ TARGET_BOOTLOADER_BOARD_NAME := chopin
 TARGET_NO_BOOTLOADER := true
 
 # HIDL
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
+#DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/mediatek_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
