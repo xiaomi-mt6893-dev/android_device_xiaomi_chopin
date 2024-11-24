5791702fcc934543e3b73a82daac29cd124ffc0b
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-11-24 13:03:54 +0000

    temp fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,10 +51,11 @@ TARGET_NO_BOOTLOADER := true
 #DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/mediatek_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml 
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
 
 # Partitions
 
