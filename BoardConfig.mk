c95f6cd226d18624fc75ebb0f11c86af3cd39acf
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-11-24 11:25:36 +0000

    chopin: Generate framework compatibility matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml 
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # Partitions
 
