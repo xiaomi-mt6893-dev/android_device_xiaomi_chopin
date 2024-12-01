accd7177560f6496f4559f32519c5918604fb116
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-12-01 08:24:23 +0000

    Revert "chopin: Build android.hardware.media.c2@1.0.so"
    This reverts commit 696406785efcbcee527e8644d93988393fa9a204.

--- device.mk
+++ device.mk
@@ -315,9 +315,6 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-	android.hardware.media.c2@1.0.vendor \
-    android.hardware.media.c2@1.1.vendor \
-    android.hardware.media.c2@1.2.vendor \
     libavservices_minijail_vendor \
     libcodec2_hidl@1.2.vendor \
     libcodec2_soft_common.vendor \
