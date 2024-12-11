439afe8c9062c4bc027d1a0e6b9fb366dbc66132
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-12-12 02:43:52 +0530

    chopin: Build 64-bit Camera Service

--- device.mk
+++ device.mk
@@ -209,8 +209,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.device@3.6.vendor \
-    android.hardware.camera.provider@2.6.vendor
+    android.hardware.camera.device@3.6.vendor:64 \
+    android.hardware.camera.provider@2.6.vendor:64
 
 # Power
 PRODUCT_PACKAGES += \
