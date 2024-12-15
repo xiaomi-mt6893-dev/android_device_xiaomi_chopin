6b6d135ad3fb076ad03bb934b2c136d2c271c533
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-12-15 20:03:41 +0000

    chopin: Configure Fingerprint Stack

--- device.mk
+++ device.mk
@@ -199,6 +199,10 @@ PRODUCT_PACKAGES += \
 	libdrm.vendor \
     libutils-v32
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6.vendor:64 \
