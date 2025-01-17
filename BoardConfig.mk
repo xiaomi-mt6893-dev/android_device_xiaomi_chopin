e1d52cbd7ae298acebe1d65359d73cd7bd1618a4
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-01-17 15:47:50 +0000

    chopin: Add Device Specific FCM and include lineage common FCM * Generated w/: https://gist.github.com/SebaUbuntu/ec053a00a9988eaea091fb1cc1f19324

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,9 @@ TARGET_NO_BOOTLOADER := true
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/mediatek_framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/mediatek_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # Partitions
 
