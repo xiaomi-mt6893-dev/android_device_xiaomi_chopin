dfc22a62ff42c6080aa2d3f937ca06d72e30f799
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-02-21 21:10:28 +0000

    chopin: Add System side IMS Props

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,7 @@ BOARD_VENDOR := xiaomi
 # Propertiesw
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 # Display
 TARGET_SCREEN_DENSITY := 440
 
