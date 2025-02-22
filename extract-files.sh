125d4229f8f41fc962cfce7e47b8c89e18a21c94
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-02-22 05:31:35 +0530

    chopin: Remove Patch for vtservice * Unwanted as we moved to newer VT blobs

--- extract-files.sh
+++ extract-files.sh
@@ -91,9 +91,6 @@ function blob_fixup {
 		system/lib64/libsink.so)
             "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
             ;;
-		 system/bin/vtservice)
-            "${PATCHELF}" --add-needed "libshim_vtservice.so" ${2}
-            ;;	
 	vendor/etc/init/init.batterysecret.rc)
             sed -i '/seclabel/d' "$2" 
 	    ;;
