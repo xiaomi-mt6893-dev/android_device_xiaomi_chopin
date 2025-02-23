a32eb5edb6ad198f9dfc38a8c51f4a976ddcbe10
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-02-23 12:51:05 +0000

    Revert "chopin: Kang overlay from `amber`"
    This reverts commit 643aad1f0366244c8ea1fd6d57545fe4277ad3a2.

--- device.mk
+++ device.mk
@@ -51,19 +51,6 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 	
- 
-# Overlays
-PRODUCT_PACKAGES += \
-    SettingsResOverlayAmber \
-    SystemUIOverlayAmber \
-    WifiOverlay \
-    CarrierConfigOverlayAmber \
-    FrameworksResOverlayAmber \
-    TelephonyResOverlayAmber \
-    TetheringResOverlayAmber
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-    
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
