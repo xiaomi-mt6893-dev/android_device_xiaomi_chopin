b40e10dbd6ab142f13ab39e5b933ab3eb157f73a
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-03-05 20:20:40 +0000

    chopin: sensors: Switch to source built subhal wrapper * Also build missing sensor service

--- device.mk
+++ device.mk
@@ -319,12 +319,15 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.chopin-multihal \
+    android.hardware.sensors@2.0-subhal-impl-1.0 \
     android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@1.0.vendor \
+    android.hardware.sensors@2.1.vendor \
     libsensorndkbridge \
     libshim_sensors
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_ODM)/etc/sensors/hals.conf
 
 PRODUCT_PACKAGES += \
 	sensors.dynamic_sensor_hal:64
