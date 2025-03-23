a66c7d647df170764c10682ba157b79d49ca4c07
bengris32 <bengris32@protonmail.ch>
2025-03-23 06:33:46 +0530

    chopin: media: Remove all duplicate references to Google C2 Codecs * These are shipped by default in AOSP since Android 14 QPR3

--- device.mk
+++ device.mk
@@ -415,10 +415,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libchrome.vendor
 
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
