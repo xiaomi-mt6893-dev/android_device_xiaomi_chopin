317cff6f4af42b1dc0fdbdfe55e13e2be0ca976f
Michael Bestas <mkbestas@lineageos.org>
2025-03-27 22:50:27 +0000

    chopin: Build Lineage Health HAL Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- device.mk
+++ device.mk
@@ -205,6 +205,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
+# Health
+PRODUCT_PACKAGES += \
+	vendor.lineage.health-service.default
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
