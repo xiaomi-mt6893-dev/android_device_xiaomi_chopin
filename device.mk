d5bb901fdfc3131d3ee95f4eb967aa62f20750e2
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-05-05 07:00:49 +0530

    [TMP] chopin: Dont Build Xiaomi Display/Touch Feature HAL for now * Until i fix sepolicy for them

--- device.mk
+++ device.mk
@@ -230,12 +230,12 @@ PRODUCT_PACKAGES += \
 	libutilscallstack.vendor
 
 # Xiaomi TouchFeature Service
-PRODUCT_PACKAGES += \
-	vendor.xiaomi.hw.touchfeature@1.0.vendor
+# PRODUCT_PACKAGES += \
+#	vendor.xiaomi.hw.touchfeature@1.0.vendor
 
 # Xiaomi DisplayFeature Service
-PRODUCT_PACKAGES += \
-	vendor.xiaomi.hardware.displayfeature@1.0.vendor 
+# PRODUCT_PACKAGES += \
+#	vendor.xiaomi.hardware.displayfeature@1.0.vendor 
 
 # Fingerprint
 PRODUCT_PACKAGES += \
